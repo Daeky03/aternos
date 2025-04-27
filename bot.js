05b23c9a42e6ac6cc0111e5417e5f5e930dfa3c6
Daeky <91830825+Daeky03@users.noreply.github.com>
2025-04-27 14:20:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,57 @@
+const mineflayer = require('mineflayer');
+const { pathfinder, Movements } = require('mineflayer-pathfinder');
+const autoeat = require('mineflayer-auto-eat');
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: "minecraft.codanime.net",
+    port: 26141,
+    username: "InsaneHuman4456"
+  });
+
+  bot.loadPlugin(pathfinder);
+  bot.loadPlugin(autoeat);
+  
+  bot.on('death', () => {
+
+    console.log('> Bot öldü, yeniden doğuyor...');
+
+    // Otomatik respawn. Eğer otomatik respawn kapalıysa bu metodu çağırın.
+
+    bot.respawn();
+
+  });
+
+  // Hata veya kopma durumunda yeniden bağlanmayı da ekleyebilirsiniz
+
+  bot.on('end', () => {
+
+    console.log('> Bağlantı koptu, yeniden bağlanılıyor...');
+
+    setTimeout(createBot, 5000);
+
+  });
+
+  bot.on('spawn', () => {
+    const moves = new Movements(bot);
+    bot.pathfinder.setMovements(moves);
+
+    bot.autoEat.options.startAt = 14;
+    bot.autoEat.enable();
+
+    // Rastgele dolaşma ve madencilik
+    setInterval(() => {
+      const goal = bot.entity.position.offset(
+        (Math.random() - 0.5) * 50,
+        0,
+        (Math.random() - 0.5) * 50
+      );
+      bot.pathfinder.setGoal({ x: goal.x, y: goal.y, z: goal.z });
+    }, 60000); // Her dakika yeni hedef
+  });
+}
+
+createBot();
+
+
 
